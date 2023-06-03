ef0b8409c14f865ee41270702566c0d17c87f927
khanimranj <108265547+khanimranj@users.noreply.github.com>
2023-06-03 23:57:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+from PIL import Image
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="IMD Weatherman")
+    st.header("Interactive weather Chat")
+    logo_image = Image.open("logo.jpg")
+
+    # Display the logo in the top left-hand side
+    st.image(logo_image, use_column_width=False, width=100)
+    
+    # upload file
+    pdf = st.file_uploader("Drop PDFs Here to Teach me", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=500,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask me a question about what I learned:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+    
+
+if __name__ == '__main__':
+    main()
